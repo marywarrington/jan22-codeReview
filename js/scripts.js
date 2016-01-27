80281701e2736738ed238215dd8707662de01e4c
Mary Warrington and Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-27 15:47:50 -0800

    add js to pingPong to replace multiples of 15 with pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -76,7 +76,9 @@
 var pingPong = function(number) {
   var results = []
   for (var i=1;i<=number;i++) {
-    if (i % 5 === 0) {
+    if (i % 15 === 0) {
+      results.push("pingpong");
+    } else if (i % 5 === 0) {
       results.push("pong");
     } else if (i % 3 === 0) {
       results.push("ping");
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,6 @@
 describe('pingPong', function() {
-  it("will count up to the user input number, and replace multiples of 3 with 'ping', multiples of 5 with 'pong'", function() {
-    expect(pingPong(5)).to.eql([1,2,"ping",4,"pong"]);
+  it("will count up to the user input number, and replace multiples of 3 with 'ping', multiples of 5 with 'pong', and multiples of 15 with 'pingpong'", function() {
+    expect(pingPong(16)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong",16]);
   });
 });
 
